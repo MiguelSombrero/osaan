075f8b16be831f6cd51b84ed6695850068ca6d08
Miika Somero <miika.somero@gmail.com>
2026-04-08 15:53:54 +0300

    upgraded Spring Boot to 4.0.5

--- microservices/competence-matching-service/src/test/java/com/github/miguelsombrero/osaan/competence_matching_service/SubscriptionMatchingServiceApplicationTests.java
+++ microservices/competence-matching-service/src/test/java/com/github/miguelsombrero/osaan/competence_matching_service/SubscriptionMatchingServiceApplicationTests.java
@@ -1,10 +1,14 @@
 package com.github.miguelsombrero.osaan.competence_matching_service;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import org.springframework.mail.javamail.JavaMailSender;
 
-@Import(TestcontainersConfiguration.class)
+@Import({TestcontainersConfiguration.class, SubscriptionMatchingServiceApplicationTests.MailTestConfiguration.class})
 @SpringBootTest
 class SubscriptionMatchingServiceApplicationTests {
 
@@ -12,4 +16,12 @@ class SubscriptionMatchingServiceApplicationTests {
     void contextLoads() {
     }
 
+    @TestConfiguration
+    static class MailTestConfiguration {
+
+        @Bean
+        JavaMailSender javaMailSender() {
+            return Mockito.mock(JavaMailSender.class);
+        }
+    }
 }
